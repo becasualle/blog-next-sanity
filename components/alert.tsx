f0f8b07da0154e47f19ac6ef911e5af513c79252
Mark <mark.nelubin@gmail.com>
2023-01-26 22:23:50 +0800

    removed basic alert option

--- components/alert.tsx
+++ components/alert.tsx
@@ -8,15 +8,10 @@ type Props = {
 
 export default function Alert({ preview }: Props) {
   return (
-    <div
-      className={cn("border-b", {
-        "bg-accent-7 border-accent-7 text-white": preview,
-        "bg-accent-1 border-accent-2": !preview,
-      })}
-    >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
+    preview && (
+      <div className="border-b bg-accent-7 border-accent-7 text-white">
+        <Container>
+          <div className="py-2 text-center text-sm">
             <>
               This page is a preview.{" "}
               <a
@@ -27,20 +22,9 @@ export default function Alert({ preview }: Props) {
               </a>{" "}
               to exit preview mode.
             </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
-      </Container>
-    </div>
+          </div>
+        </Container>
+      </div>
+    )
   );
 }
