5190ad7a76a95081d5f9dbd220449878a706c396
Mark <mark.nelubin@gmail.com>
2023-01-27 23:00:46 +0800

    added branding

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,7 +2,7 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl lg:text-7xl font-bold tracking-tighter leading-tight">
-        Марк Нелюбин.
+        DevBlast <span className="text-4xl lg:text-[3.5rem]">☄️</span>
       </h1>
       <h4 className="text-center md:text-left text-lg">
         Блог про дизайн, разработку, менеджмент
