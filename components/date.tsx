826ca34f6900a61bcbdf24a9007c0caf6c81c727
Mark <mark.nelubin@gmail.com>
2023-02-15 17:13:22 +0800

    Added dark theme styles

--- components/date.tsx
+++ components/date.tsx
@@ -4,5 +4,9 @@ export default function Date({ dateString }) {
   if (!dateString) return null;
 
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  return (
+    <time dateTime={dateString} className="dark:text-slate-400">
+      {format(date, "LLLL	d, yyyy")}
+    </time>
+  );
 }
