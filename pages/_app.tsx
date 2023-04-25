5fac11d010effeab1e3438919b2191a81217d24e
Mark <mark.nelubin@gmail.com>
2023-04-25 16:43:34 +0700

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import "../styles/index.css";
-import React from "react";
-import { AppProps } from "next/app";
+import '../styles/index.css';
+import React from 'react';
+import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
